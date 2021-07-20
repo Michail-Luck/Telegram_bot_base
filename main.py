96b90de2f928338fcbe48e2d0ab958f25e8fe25e
us8 <sarafan2517@gmail.com>
2021-07-20 17:13:55 +0500

    Simple work bot

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import requests
+import time
 
 TOKEN ='1780674470:AAH6Iuyeyuf4eshxGSbHsVyvXQIYG13NsI4'
 
 BOT_URL = f'https://api.telegram.org/bot{TOKEN}'
 
-url = f"{BOT_URL }/getMe"
+url = f"{BOT_URL }/getUpdates"
 
 result = requests.get(url)
 
 print(result.status_code)
+
+while True:
+    time.sleep(3)
+    result = requests.get(url)
+    messages = result.json()['result']
+
+
+    for message in messages:
+        chat_id = message['message']['chat']['id']
+        url_send = f"{BOT_URL }/sendMessage"
+        params = {
+            'chat_id': chat_id ,
+            'text': 'You where ?'
+        }
+        answer = requests.post(url_send, params = params)
+
